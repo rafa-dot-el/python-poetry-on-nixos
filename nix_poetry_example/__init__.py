8a71a8736793039eec41073d097c66153ad770c7
Rafael <104688305+rafa-dot-el@users.noreply.github.com>
2023-03-18 11:10:59 +0100

    Initial

---
+++ nix_poetry_example/__init__.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+
+def sum(a, b):
+    return a + b
---
+++ tests/test_example.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+from unittest import TestCase
+
+
+class TestExampleNixTest(TestCase):
+    def test_can_import_something_with_native_deps(self):
+        try:
+            import nix_poetry_example as example
+
+            self.assertTrue(True)
+        except Exception as e:
+            self.assertEqual(e, "", "Expected to load library but got " + e)
